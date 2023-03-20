c69a4b871cd4bc0c411e55df368e5e6ded094a94
baize <1198047287@qq.com>
2023-03-20 15:19:18 +0800

    基础搭建完成

--- xinxuan-product/src/main/java/com/baize/mall/product/controller/CategoryController.java
+++ xinxuan-product/src/main/java/com/baize/mall/product/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.baize.mall.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 
@@ -32,14 +33,13 @@ public class CategoryController {
     private CategoryService categoryService;
 
     /**
-     * 列表
+     * 查出所有树形分类
      */
-    @RequestMapping("/list")
+    @RequestMapping("/list/tree")
     ////@RequiresPermissions("product:category:list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
-
-        return R.ok().put("page", page);
+    public R list(){
+        List<CategoryEntity> categoryEntities =  categoryService.listWithTree();
+        return R.ok().put("data",categoryEntities);
     }
 
 
--- xinxuan-product/src/main/java/com/baize/mall/product/service/impl/CategoryServiceImpl.java
+++ xinxuan-product/src/main/java/com/baize/mall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,11 @@
 package com.baize.mall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -25,5 +29,30 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
         return new PageUtils(page);
     }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        List<CategoryEntity> categoryEntities = baseMapper.selectList(null);
+        List<CategoryEntity> parents = categoryEntities.stream().filter(item -> item.getParentCid() == 0)
+                // Map 是把每一个遍历的。进行设置
+                // 此处设置每个的子菜单
+                .map(menu->{
+                    menu.setChildren(getChildren(menu,categoryEntities));
+                    return menu;
+                }).collect(Collectors.toList());
+        return parents;
+    }
+
+    private List getChildren(CategoryEntity root, List<CategoryEntity> categoryEntities) {
+        List<CategoryEntity> collect = categoryEntities.stream().filter(categoryEntity -> {
+            return (categoryEntity.getParentCid() == root.getCatId());
+            // 此处进行递归查找，如果有子菜单，则继续递归调用
+        }).map((menu)->{
+            menu.setChildren(getChildren(menu,categoryEntities));
+            return menu;
+        }).collect(Collectors.toList());
+        return collect;
+    }
+
 
 }
